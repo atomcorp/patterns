846355b3e7e0c321953c8206af0cbb2747d9c91e
atomcorp <thomasmaxwellsmith@gmail.com>
2019-04-13 17:16:10 +0100

    repeatable pattern

--- src/pages/RepeatedPattern.tsx
+++ src/pages/RepeatedPattern.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import {Page} from '../components';
+import {Page, Board} from '../components';
+
+const pattern = [['🦕', '🦖'], ['🦖', '🦕']];
+
+const gridWithPattern = (x: number, y: number, pattern: string[][]) => {
+  const patternRowLength = pattern.length;
+  const patternColLength = pattern[0].length;
+  return Array(y)
+    .fill(null)
+    .map((_, rowIndex) =>
+      Array(x)
+        .fill(null)
+        .map(
+          (_, colIndex) =>
+            pattern[rowIndex % patternRowLength][colIndex % patternColLength]
+        )
+    );
+};
+
+const matrix = gridWithPattern(8, 6, pattern);
 
 const RepeatedPattern = () => (
-  <Page title="Repeated pattern">RepeatedPattern</Page>
+  <Page title="Repeated pattern">
+    Pattern:
+    <Board matrix={pattern} />
+    <br />
+    Repeated pattern:
+    <Board matrix={matrix} />
+  </Page>
 );
 
 export default RepeatedPattern;
